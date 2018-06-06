de33be97521c924ae0fea73d380b5b28825a683b
gayhub <gayhub@live.cn>
2018-06-06 16:34:09 +0800

    mybatis cache

--- src/main/java/com/whh/spring/boot/SpringBoot2Application.java
+++ src/main/java/com/whh/spring/boot/SpringBoot2Application.java
@@ -5,8 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 @MapperScan("com.whh.spring.boot.dao") //mapper 扫描
 public class SpringBoot2Application {
 
