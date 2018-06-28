61e5203533acb8938a36d7d15313f91861773802
gayhub <gayhub@live.cn>
2018-06-28 11:33:03 +0800

    lombok

---
+++ src/main/java/com/whh/spring/boot/model/LombokDemo.java
@@ -0,0 +1,15 @@
+package com.whh.spring.boot.model;
+
+import lombok.Data;
+
+/** lombok demo
+ * @author huahui.wu
+ * @date 2018/6/28 09:54
+ * @description
+ */
+@Data
+public class LombokDemo {
+
+    private String name;
+
+}
